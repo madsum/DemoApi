daff39704d4836e856bb4b9cc561c88addc5192d
Masum Islam <muhammed.islam@ericsson.com>
2020-06-03 19:05:02 +0300

    Add Junit test and PlantRestController

---
+++ src/main/java/com/example/demo/controller/PlantRestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Plant;
+import com.example.demo.service.PlantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PlantRestController {
+
+    @Autowired
+    PlantService plantService;
+
+    @GetMapping("/getAllPlant")
+    public List<Plant> getPlant(@RequestParam(required = false) String from,
+                           @RequestParam(required = false) String to){
+        return plantService.getJsonObject(from, to);
+    }
+
+}
